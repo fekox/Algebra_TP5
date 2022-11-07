651432dd9f3ce3ff75fa9e77cf87fe626f513e02
Mau <mauvepinktree@gmail.com>
2022-11-07 10:41:56 -0300

    Implemented basic program functionality, defined main classes

---
+++ src/game.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace Pool {
+	namespace Game {
+		void runGame();
+	}
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,28 @@
-#include "raylib.h"
+#include "game.h"
 
 const int screenWidth = 800;
 const int screenHeight = 600;
 
 int main() {
+	/*
 	// Init
 	InitWindow(screenWidth, screenHeight, "Algebra TP5");
+
 
 	while (!WindowShouldClose()) {
 		// Update
 
-
 		// Draw
 		BeginDrawing();
 		ClearBackground(BLACK);
-		
 
 		EndDrawing();
 
 	}
 	// Close
 	CloseWindow();
+	*/
+	Pool::Game::runGame();
 
 	return 0;
 }
