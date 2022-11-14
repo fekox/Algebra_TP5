51bd1e541338595582e71c8bb21afc878b125355
Mau <mauvepinktree@gmail.com>
2022-11-14 09:33:42 -0300

    Agregado colision contra los bordes de la pantalla

--- src/Objects/Ball.cpp
+++ src/Objects/Ball.cpp
@@ -9,12 +9,32 @@ using namespace std;
 // Private
 
 void Move(Ball& ball);
+void HandleBounce(Ball& ball);
 
 // --
 
 void Move(Ball& ball) {
 	ball.pos.x += (ball.acceleration.x * GetFrameTime()) * 200.0f;
 	ball.pos.y += (ball.acceleration.y * GetFrameTime()) * 200.0f;
+}
+
+void HandleBounce(Ball& ball) {
+	if (ball.pos.x - ball.radius <= 0) {
+		ball.pos.x = ball.radius + 1;
+		ball.acceleration.x = -ball.acceleration.x;
+	}
+	if (ball.pos.x + ball.radius >= GetScreenWidth()) {
+		ball.pos.x = GetScreenWidth() - ball.radius - 1;
+		ball.acceleration.x = -ball.acceleration.x;
+	}
+	if (ball.pos.y - ball.radius <= 0) {
+		ball.pos.y = ball.radius + 1;
+		ball.acceleration.y = -ball.acceleration.y;
+	}
+	if (ball.pos.y + ball.radius >= GetScreenHeight()) {
+		ball.pos.y = GetScreenHeight() - ball.radius - 1;
+		ball.acceleration.y = -ball.acceleration.y;
+	}
 }
 
 // Public
@@ -49,6 +69,7 @@ void ShootBall(Ball& ball, Vector2 target) {
 
 void UpdateBall(Ball& ball) {
 	Move(ball);
+	HandleBounce(ball);
 }
 
 void DrawBall(Ball mainBall)
